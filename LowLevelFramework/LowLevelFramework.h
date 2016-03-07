83147611b4232bab26a25d57158865b673b6b0d2
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-03-07 17:07:59 +1100

    Initial Commit

---
+++ LowLevelFramework/LowLevelFramework.h
@@ -0,0 +1,19 @@
+//
+//  LowLevelFramework.h
+//  LowLevelFramework
+//
+//  Created by Giovanni Lodi on 7/03/2016.
+//  Copyright © 2016 Giovanni Lodi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LowLevelFramework.
+FOUNDATION_EXPORT double LowLevelFrameworkVersionNumber;
+
+//! Project version string for LowLevelFramework.
+FOUNDATION_EXPORT const unsigned char LowLevelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LowLevelFramework/PublicHeader.h>
+
+
