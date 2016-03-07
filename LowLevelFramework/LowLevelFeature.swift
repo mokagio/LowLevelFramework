a17d137ae9b3b7f894f7e1dab9ee777d0b1ab9f3
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-03-07 17:23:00 +1100

    Create a demo project to play around with dependency resolutions

---
+++ LowLevelFramework/LowLevelFeature.swift
@@ -0,0 +1,13 @@
+//
+//  LowLevelFeature.swift
+//  LowLevelFramework
+//
+//  Created by Giovanni Lodi on 7/03/2016.
+//  Copyright © 2016 Giovanni Lodi. All rights reserved.
+//
+
+import Foundation
+
+public func lowLevelFeature() -> String {
+    return "low-level-result"
+}
---
+++ LowLevelFrameworkTests/LowLevelFeatureTest.swift
@@ -0,0 +1,17 @@
+//
+//  LowLevelFeatureTest.swift
+//  LowLevelFramework
+//
+//  Created by Giovanni Lodi on 7/03/2016.
+//  Copyright © 2016 Giovanni Lodi. All rights reserved.
+//
+
+import XCTest
+@testable import LowLevelFramework
+
+class LowLevelFeatureTest: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(lowLevelFeature(), "low-level-result")
+    }
+}
